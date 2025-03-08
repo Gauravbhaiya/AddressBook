16d2f15764da7291438f2c57d88b4a95be056b24
Gauravbhaiya <gauravkosi78@gmail.com>
2025-03-08 23:35:40 +0530

    Logging

---
+++ AddressBookApp-UC8_Logging/src/main/java/com/AddressBookApp/controller/ContactController.java
@@ -0,0 +1,54 @@
+package com.AddressBookApp.controller;
+
+import com.AddressBookApp.dto.ContactDTO;
+import com.AddressBookApp.model.Contact;
+import com.AddressBookApp.service.ContactService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/contacts")
+public class ContactController {
+
+    ContactService contactService;
+
+    public ContactController(ContactService contactService) {
+        this.contactService = contactService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Contact>> getAllContacts() {
+        log.info("Received request to fetch all contacts");
+        return ResponseEntity.ok(contactService.getAllContacts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Contact> getContactById(@PathVariable Long id) {
+        log.info("Received request to fetch contact with ID: {}", id);
+        return ResponseEntity.ok(contactService.getContactById(id));
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Contact> addContact(@RequestBody Contact contact) {
+        log.info("Received request to add a new contact");
+        return ResponseEntity.ok(contactService.addContact(contact));
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Contact> updateContact(@PathVariable Long id, @RequestBody Contact contact) {
+        log.info("Received request to update contact with ID: {}", id);
+        return ResponseEntity.ok(contactService.updateContact(id, contact));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteContact(@PathVariable Long id) {
+        log.info("Received request to delete contact with ID: {}", id);
+        contactService.deleteContact(id);
+        return ResponseEntity.noContent().build();
+    }
+}
